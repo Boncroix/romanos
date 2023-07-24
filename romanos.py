2ed2304892b88978a3fe987aa43674d88eddb624
Jose Bueno Cruz <josebuenocruz@MacBook-Air-de-Jose.local>
2023-07-24 22:06:00 +0200

    feat: función convertir a romano y comprobar entero

---
+++ romanos.py
@@ -0,0 +1,14 @@
+def convertir_a_romano(numero):
+    try:
+        num = int(numero)
+        return 'TODO: convertir a romano'
+    except ValueError:
+        print(f'Error: debes introducir un número entero ({numero})')
+        return ''
+
+
+""""
+convertir_a_romano()
+convertir_a_romano(56)
+convertir_a_romano('lo que quiera')
+"""
