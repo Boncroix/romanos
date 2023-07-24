abec57c4ef1e1ae638c2271fc0a548c9bb2af9fe
Boncroix <boncroix@gmail.com>
2023-07-24 23:29:48 +0200

    añadir listas y calcular resto

--- romanos.py
+++ romanos.py
@@ -7,3 +7,17 @@ def convertir_a_romano(numero):
     if not (0 < numero < 4000):
         return f'Error: el número debe estar entre 1 y 3999 ({numero})'
     return 'TODO: convertir a romano'
+
+    millares = ['', 'M', 'MM', 'MMM']
+    centenas = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+    decenas = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+    unidades = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+
+    millar = numero // 1000
+    centena = (numero % 1000) // 100
+    decena = ((numero % 1000) % 100) // 10
+    unidad = (((numero % 1000) % 100) % 10) // 1
+
+    romano = millares[millar], centenas[centena], decenas[decena], unidad[unidad]
+
+    return romano
