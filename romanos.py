46be907f390e0bbb62e45d373e0d36cf8a825bd8
Boncroix <boncroix@gmail.com>
2023-07-24 23:50:30 +0200

    eliminar return cuando el número no está en limite

--- romanos.py
+++ romanos.py
@@ -1,12 +1,11 @@
 def convertir_a_romano(numero):
     # Comprobar número entero
-    if isinstance(numero, (int, float)):
+    if not isinstance(numero, (int, float)):
         print(f'Error: debes introducir un número entero ({numero})')
 
     # Comprobar que número está entre 2 números
     if not (0 < numero < 4000):
         return f'Error: el número debe estar entre 1 y 3999 ({numero})'
-    return 'TODO: convertir a romano'
 
     millares = ['', 'M', 'MM', 'MMM']
     centenas = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
@@ -21,3 +20,7 @@ def convertir_a_romano(numero):
     romano = millares[millar], centenas[centena], decenas[decena], unidad[unidad]
 
     return romano
+
+
+numero_romano = convertir_a_romano(5)
+print(numero_romano)
