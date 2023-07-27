19e8f8a15de2d08bbfccd943ae80468631f63f40
Boncroix <boncroix@gmail.com>
2023-07-27 18:36:43 +0200

    feat: copiar ejercicio clase anterior y añadir excepciones y que una letra no se pueda repetir más de 3 veces

--- romanos.py
+++ romanos.py
@@ -1,61 +1,90 @@
+
 def convertir_a_romano(numero):
-    # Comprobar número entero
-    if not isinstance(numero, (int, float)):
-        print(f'Error: debes introducir un número entero ({numero})')
+    if type(numero) != int:
+        return f"Error: debes introducir un número entero ({numero})"
 
-    # Comprobar que número está entre 2 números
+    # validar el valor del número
     if not (0 < numero < 4000):
-        return f'Error: el número debe estar entre 1 y 3999 ({numero})'
+        return f"Error: el número debe estar entre 1 y 3999 ({numero})"
 
-    millares = ['', 'M', 'MM', 'MMM']
-    centenas = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
-    decenas = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
-    unidades = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+    conversores = [
+        ["", "M", "MM", "MMM"],
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ]
 
-    millar = numero // 1000
-    centena = (numero % 1000) // 100
-    decena = ((numero % 1000) % 100) // 10
-    unidad = (((numero % 1000) % 100) % 10) // 1
+    divisores = [1000, 100, 10, 1]
 
-    romano = millares[millar], centenas[centena], decenas[decena], unidad[unidad]
+    resultado = ""
+    contador = 0
 
-    return romano
+    for divisor in divisores:
+        cociente = (numero // divisor)
+        numero = numero % divisor
+        resultado = resultado + conversores[contador][cociente]
+        contador = contador + 1
+
+    return resultado
 
 
 def romano_a_entero(romano):
-    digitos_romanos = {'I': 1,
-                       'V': 5,
-                       'X': 10,
-                       'L': 50,
-                       'C': 100,
-                       'D': 500,
-                       'M': 1000
-                       }
+    digitos_romanos = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
 
     if not isinstance(romano, str):
-        return 'ERROR: tiene que ser un número romano en formato cadena de texto'
+        raise TypeError(
+            'ERROR: tiene que ser un número romano en formato cadena de texto')
 
     resultado = 0
     anterior = 0
+
     for letra in romano:
+
+        letra_repetida = romano.count(letra)
+        if letra_repetida > 3:
+            raise ValueError(
+                'ERROR: Numero romano no valido letra repetida más de 3 veces')
+
         if letra not in digitos_romanos:
-            return f'ERROR: {letra} no es un dígito romano válido (I, V, X, L, C, D, M)'
+            raise ValueError(
+                f'ERROR: {letra} no es un dígito romano válido (I, V, X, L, C, D, M)')
         actual = digitos_romanos[letra]
 
         if anterior < actual:
-            # deshacer la suma  que hemos hecho en la iteración anterior
+            # comprobar que la resta es posible
+            # el orden de magnitud no es mayor de uno
+
+            if anterior > 0 and len(str(actual)) - len(str(anterior)) > 1:
+                raise ValueError(
+                    f"ERROR: resta no posible (ant: {anterior}, act: {actual})")
+
+            # deshacer la suma (que hemos hecho en la iteración anterior)
+            # DCI --- DC
             resultado = resultado - anterior
+            # DC(IV) 600 + 4 = 600 + (5 - 1) = resultado + (actual - anterior)
             # sumar el valor actual, pero restando el anterior
             resultado = resultado + (actual - anterior)
         else:
-            resultado = resultado + anterior
+            resultado = resultado + actual
 
         anterior = actual
 
     return resultado
 
 
-errores = ['A', '', 4, ['X', 'X', 'I'], 'IV']
-pruebas = ['I', 'MMMCMXCIX']
-for valor in errores:
+errores = ['A', '', 3, ['X', 'X', 'I']]
+pruebas = ['IIII', 'I', 'MCXXIII', 'VIII', 'LVI', 'IV',
+           'IX', 'XC', 'CM', 'IC', 'IM', 'XM', 'ID', 'VX']
+
+
+for valor in pruebas:
     print(romano_a_entero(valor))
+# print(romano_a_entero('MCXCIV'))
